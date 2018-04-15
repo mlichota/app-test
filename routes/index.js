0a30da68ed235032c7aa0afa3bd29da481b9631d
lismulder <lichota.michal@gmail.com>
2018-04-16 01:51:22 +0200

    https, temporary links

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fileUpload = require('express-fileupload');
 var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
+var s3 = new AWS.S3({ endpoint: 's3-us-east-2.amazonaws.com', signatureVersion: 'v4', region: 'us-east-2' });
 var fs = require('fs');
 var dataFormated = [];
 var path = require('path');
@@ -10,6 +10,7 @@ var filePath;
 var fileName;
 var emptyBucket;
 var session = require('express-session');
+var clipboardy = require('clipboardy');
 
 
 
@@ -21,7 +22,7 @@ router.get('/', ensureAuthenticated, function (req, res) {
 
 		req.session.tmpDir = folder;
 		console.log('Tworzę folder tymczasowy: ' + req.session.tmpDir)
-		
+
 	});
 
 
@@ -63,7 +64,7 @@ router.get('/upload', ensureAuthenticated, function (req, res) {
 							} else {
 
 								console.log("Successfully uploaded data to myBucket/myKey");
-								req.flash('success_msg', 'Plik: ' + filename + ' został załadowany!');	
+								req.flash('success_msg', 'Plik: ' + filename + ' został załadowany!');
 								res.redirect('upload');
 
 							}
@@ -72,7 +73,7 @@ router.get('/upload', ensureAuthenticated, function (req, res) {
 					});
 
 				});
-				
+
 
 			}
 			else {
@@ -83,10 +84,10 @@ router.get('/upload', ensureAuthenticated, function (req, res) {
 		else {
 			req.flash('error_msg', 'Nie wybrałeś pliku. Wybierz plik i spróbuj ponownie.');
 			res.redirect('upload');
-			
+
 		}
 
-		
+
 
 	});
 
@@ -147,6 +148,27 @@ router.get('/download-file-s3', ensureAuthenticated, function (req, res) {
 	s3.getObject(params).createReadStream().pipe(file);
 
 });
+
+router.get('/share-file', ensureAuthenticated, function (req, res) {
+	fileName = req.query.file;
+
+
+
+
+	var params = { Bucket: 'mlichota-test-' + req.user.username, Key: fileName, Expires: 300 };
+	var url = s3.getSignedUrl('getObject', params, function (err, url) {
+		console.log('The URL is', url);
+		clipboardy.writeSync(url);
+		req.flash('success_msg', 'Tymczasowy link do pliku: "' + fileName + '" został skopiowany do schowka. Link będzie aktywny przez 5 minut.');
+		res.redirect('download');
+
+	});
+	// expires in 60 seconds
+
+
+
+});
+
 
 function ensureAuthenticated(req, res, next) {
 	if (req.isAuthenticated()) {
