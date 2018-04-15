ab6ecb1897a413f03c58a835d8940e362327fe75
lismulder <lichota.michal@gmail.com>
2018-04-15 21:15:08 +0200

    0.0.1

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.use('/users', users);
 
 
 // Set Port
-app.set('port', (process.env.PORT || 80));
+app.set('port', (process.env.PORT || 8000));
 
 app.listen(app.get('port'), function(){
 	console.log('Server started on port '+app.get('port'));
